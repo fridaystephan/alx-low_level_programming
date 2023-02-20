6be5d8d7e854a22fdf90a9ce7de1a93083a071ce
Friday Abiona Stephen <fridaystephan@gmail.com>
2023-02-20 12:05:42 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,15 @@
 #include <unistd.h>
 
-int main(void) {
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 1 (Error occurred)
+ */
+int main(void)
+{
     char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-    write(STDERR_FILENO, str, sizeof(str)-1);
-    return 1;
+
+    write(STDERR_FILENO, str, sizeof(str) - 1);
+
+    return (1);
 }
