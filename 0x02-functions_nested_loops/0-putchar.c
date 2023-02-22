fe8ca6ba5d39b76faa8bb7bffd7f781d8cb69a7f
Friday Abiona Stephen <fridaystephan@gmail.com>
2023-02-22 11:36:37 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "printputchar.h"
+#include "main.h"
 /**
  * main - Entry point
  *
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	char *sh = "printputchar";
+	char *sh = "Main";
 
 	while (*sh)
 	{
