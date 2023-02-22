21e2b2dad820780726c26a3777887f2f1998f1ee
Friday Abiona Stephen <fridaystephan@gmail.com>
2023-02-22 12:18:12 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,18 @@
 #include "main.h"
+#include <stdio.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+    return putchar(c);
+}
+
 /**
  * main - Entry point
  *
@@ -6,14 +20,15 @@
  */
 int main(void)
 {
-	char *sh = "Main";
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+    return (0);
 }
